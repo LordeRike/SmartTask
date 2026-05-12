9711c5afa03442bb61933136cb3de51d4bd66c5d
Henrique <henrike.costaa@gmail.com>
2026-05-12 20:03:32 -0300

    feat: adicionar sistema de prioridade

--- Tarefa.java
+++ Tarefa.java
@@ -1,9 +1,11 @@
 public class Tarefa {
     String descricao;
     boolean concluida;
+    String prioridade; // Novo Atributo
 
     public Tarefa(String descricao) {
         this.descricao = descricao;
         this.concluida = false;
+        this.prioridade = "Baixa"; //Valor padrao
     }
 }
