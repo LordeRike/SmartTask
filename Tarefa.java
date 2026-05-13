db3776e9307b30eaaf515570bf274b32c00aeb3c
Henrique <henrike.costaa@gmail.com>
2026-05-12 21:22:00 -0300

    add prazo de entrega

--- Tarefa.java
+++ Tarefa.java
@@ -2,9 +2,18 @@ public class Tarefa {
     String descricao;
     boolean concluida;
     String prioridade; // Novo Atributo
+    String dataEntrega;
 
     public Tarefa(String descricao) {
         this.descricao = descricao;
         this.concluida = false;
+        this.prioridade = "Baixa"; //Valor padrao
+        
     }
+
+    public void dataEntrega(String dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    
 }
